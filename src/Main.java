9e14cb2ab0d0ae30e39c87c6199cfcf4812cb181
mycomputer <Danil12.07.2012@gail.com>
2023-01-10 19:57:37 +0300

    Tasks 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int clientOS = 0;
-        if (clientOS == 0) {
+        int clientDeviceYear = 2016;
+        if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        }else if (clientDeviceYear >= 2015 && clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
-        } else { System.out.println("Установите версию приложения для Android по ссылке.");
+        } else if (clientDeviceYear < 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+
+        }else {
+            System.out.println("Установите версию приложения для Android по ссылке.");
         }
     }
 }
+
