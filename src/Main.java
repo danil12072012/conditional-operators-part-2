c36d3b7d6e8d061cde6f1005d5b7ce58282d4ad4
mycomputer <Danil12.07.2012@gail.com>
2023-01-10 21:21:50 +0300

    Tasks 5

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        int deliveryDistance = 95;
-        int deliveryTimeIsOneDay = 1;
-        int deliveryTimeIsTwoDays = deliveryTimeIsOneDay + 1;
-        int deliveryTimeIsThreeDays = deliveryTimeIsTwoDays + 1;
-        if ( deliveryDistance <= 20) {
-            System.out.println("Потребуется дней: " + deliveryTimeIsOneDay);
-        }else if (deliveryDistance>20 && deliveryDistance<=60) {
-            System.out.println("Потребуется дней: " + deliveryTimeIsTwoDays);
-        }else if (deliveryDistance>60 && deliveryDistance<=100) {
-            System.out.println("Потребуется дней: " + deliveryTimeIsThreeDays);
-        } else {
-            System.out.println("К сожалению доставки нет.");
+        int monthNumber = 13;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима.");
+                break;
+            case 3, 4, 5:
+                System.out.println("Весна");
+                break;
+            case 6, 7, 8:
+                System.out.println("Лето");
+                break;
+            case 9, 10,  11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца нет.");
+
+
         }
 
 
