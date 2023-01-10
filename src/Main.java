5e22ead72cb34df23c3d8bf01ab6adfdd8a747aa
mycomputer <Danil12.07.2012@gail.com>
2023-01-10 20:55:54 +0300

    Tasks 4

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int year = 2024;
-        if ((year%4==0 || year%400==0) && year%100!=0) {
-            System.out.println(year + " год яляется високосным");
-        } else System.out.println(year + " год не является високосным.");
+        int deliveryDistance = 95;
+        int deliveryTimeIsOneDay = 1;
+        int deliveryTimeIsTwoDays = deliveryTimeIsOneDay + 1;
+        int deliveryTimeIsThreeDays = deliveryTimeIsTwoDays + 1;
+        if ( deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: " + deliveryTimeIsOneDay);
+        }else if (deliveryDistance>20 && deliveryDistance<=60) {
+            System.out.println("Потребуется дней: " + deliveryTimeIsTwoDays);
+        }else if (deliveryDistance>60 && deliveryDistance<=100) {
+            System.out.println("Потребуется дней: " + deliveryTimeIsThreeDays);
+        } else {
+            System.out.println("К сожалению доставки нет.");
+        }
+
+
     }
 }
 
