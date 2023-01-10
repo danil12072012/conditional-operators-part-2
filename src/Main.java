fe8aa2e954904dbaacbbe7184d441a4b94468d10
mycomputer <Danil12.07.2012@gail.com>
2023-01-10 19:35:21 +0300

    Tasks 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else { System.out.println("Установите версию приложения для Android по ссылке.");
+        }
     }
 }
